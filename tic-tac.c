62a4307792b052de9027b5ea8b78f70189413fc7
Ayobami <ayobamidele006@gmail.com>
2022-12-26 17:52:10 +0100

    [Update]: updated the tic-tac game to get winner

--- tic-tac.c
+++ tic-tac.c
@@ -4,8 +4,7 @@
 char board[3][3] = {
     {' ', ' ', ' '},
     {' ', ' ', ' '},
-    {' ', ' ', ' '}
-};
+    {' ', ' ', ' '}};
 
 char player = 'X';
 
@@ -42,12 +41,58 @@ void player_turn()
             player = 'X';
     }
 }
+int checkwin()
+{
+    if (board[0][0] != ' ' && board[0][0] == board[0][1] && board[0][1] != ' ' && board[0][1] == board[0][2])
+    {
+        printf("Player %c wins \n", board[0][2]);
+        exit(0);
+    }
+    else if (board[1][0] != ' ' && board[1][0] == board[1][1] && board[1][1] != ' ' && board[1][1] == board[1][2])
+    {
+        printf("Player %c wins \n", board[1][2]);
+        exit(0);
+    }
+    else if (board[2][0] != ' ' && board[2][0] == board[2][1] && board[2][1] != ' ' && board[2][1] == board[2][2])
+    {
+        printf("Player %c wins \n", board[2][2]);
+        exit(0);
+    }
+    else if (board[0][0] != ' ' && board[0][0] == board[1][0] && board[1][0] != ' ' && board[1][0] == board[2][0])
+    {
+        printf("Player %c wins \n", board[2][0]);
+        exit(0);
+    }
+    else if (board[0][1] != ' ' && board[0][1] == board[1][1] && board[1][1] != ' ' && board[1][1] == board[2][1])
+    {
+        printf("Player %c wins \n", board[2][1]);
+        exit(0);
+    }
+    else if (board[0][2] != ' ' && board[0][2] == board[1][2] && board[1][2] != ' ' && board[1][2] == board[2][2])
+    {
+        printf("Player %c wins \n", board[2][2]);
+        exit(0);
+    }
+    else if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] != ' ' && board[1][1] == board[2][2])
+    {
+        printf("Player %c wins \n", board[2][2]);
+        exit(0);
+    }
+    else if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] != ' ' && board[1][1] == board[2][0])
+    {
+        printf("Player %c wins \n", board[2][0]);
+        exit(0);
+    }
+
+    return (0);
+}
 
 int main()
 {
     draw_board();
     while (1)
     {
+        checkwin();
         player_turn();
         draw_board();
     }
