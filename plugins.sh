5cc54d5d0af2a42927e135f45e773820cb2f8194
Ayobami <ayobamidele006@gmail.com>
2023-08-18 08:40:31 +0100

    hardcode omz path for cloning destinatio

--- plugins.sh
+++ plugins.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
 # install syntax plugins and autosuggestion
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 # search and replace
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-source ~/.zshrc 
+
+read -p "Do you want to set vi as alias for nvim? (Y/n)" ans
+
+if [ $ans == "y" ] || [ $ans == "Y" ]; then
+	alias vi="nvim"
+	echo "You can now open your nvim with vi"
+fi
